c73bf3163014bb72950ac405f7b3920575acd2e3
TechyDi <dkd1210202gmail.com>
2024-08-15 19:28:06 +0530

    add the 1st sub

--- percentagecal.java
+++ percentagecal.java
@@ -2,6 +2,13 @@ import java.util.*;
 public class percentagecal {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("WLCOME TO PERCENTAGE CALCULATOR.");
+        System.out.println("Enter your marks of Subject-1 : ");
+            int sub1 = sc.nextInt();
+
+        
+    sc.close();
     }
 }
