871955570f59b86c9a2ea581c7b3f8ff1ee9093c
TechyDi <dkd1210202gmail.com>
2024-08-15 21:29:40 +0530

    Well designed

--- percentagecal.java
+++ percentagecal.java
@@ -9,57 +9,60 @@ public class percentagecal {
         System.out.println("WLCOME TO PERCENTAGE CALCULATOR.");
         System.out.println("NOW ADD YOUR MARKS OF EACH SUBJECTS(eg.95 out of 100).");
 
-        while(true){
-            System.out.println("Enter your marks of Subject-1 : ");
-            sub1 = sc.nextFloat();
+            while(true){
+                System.out.println("Enter your marks of Subject-1 : ");
+                sub1 = sc.nextFloat();
 
-            if(sub1<=100)
-            break;
-            else{
+                    if(sub1<=100)
+                 break;
+                    else{
                 System.out.println("Invalid Marks. Try Again.");
             }
-            
         }   
-        while(true){
-            System.out.println("Enter your marks of Subject-2 : ");
-            sub2 = sc.nextFloat();
-            if(sub2<=100)
-            break;
-            else{
+            while(true){
+                System.out.println("Enter your marks of Subject-2 : ");
+                sub2 = sc.nextFloat();
+
+                    if(sub2<=100)
+                break;
+                    else{
                 System.out.println("Invalid Marks. Try Again.");
             }
         }
-        while(true){
-            System.out.println("Enter your marks of Subject-3 : ");
-            sub3 = sc.nextFloat();    
-            if(sub3<=100)
-            break;
-            else{
+            while(true){
+                System.out.println("Enter your marks of Subject-3 : ");
+                sub3 = sc.nextFloat(); 
+
+                    if(sub3<=100)
+                break;
+                    else{
                 System.out.println("Invalid Marks. Try Again.");
             }
         }
-        while(true){
-            System.out.println("Enter your marks of Subject-4 : ");
-            sub4 = sc.nextFloat();
-            if(sub4<=100)
-            break;
-            else{
+            while(true){
+                System.out.println("Enter your marks of Subject-4 : ");
+                sub4 = sc.nextFloat();
+
+                    if(sub4<=100)
+                break;
+                    else{
                 System.out.println("Invalid Marks. Try Again.");
             }
         }
-        while(true){
-            System.out.println("Enter your marks of Subject-5 : ");
-         sub5 = sc.nextFloat();  
-            if(sub5<=100)
-            break;
-            else{
+            while(true){
+                System.out.println("Enter your marks of Subject-5 : ");
+                sub5 = sc.nextFloat();  
+
+                    if(sub5<=100)
+                break;
+                    else{
                 System.out.println("Invalid Marks. Try Again.");
             }
         }
         
-        float sum = sub1+sub2+sub3+sub4+ sub5 ;   
-        float per = sum/500 * 100 ; 
-        System.out.println("Your Percentage is " + per +" %");
+        // float sum = sub1+sub2+sub3+sub4+ sub5 ;   
+        // float per = sum/500 * 100 ; 
+        System.out.println("Your Percentage is " + (sub1+sub2+sub3+sub4+ sub5)/500 * 100 +" %");
 
     sc.close();
         }
